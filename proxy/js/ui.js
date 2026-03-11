wolfy1339/OpenRailwayMap-vector

commit 160810aecb0953edfb52a606732dcb1924171066

Import and show more station references (#832)
Part of #441

Fixes https://github.com/hiddewie/OpenRailwayMap-vector/issues/819

Import of references. They are the references with 200 or more usages
worldwide from the list in
https://github.com/hiddewie/OpenRailwayMap-vector/issues/441#issuecomment-3977455862.

Implementation: store all references in a hash in the stations table, to
ensure they can be searched generally.

The station popup shows all station references, sorted by importance
(order in the definition list).

Search searches all known references, and the search results show the
references next to the station names. The search option for UIC
reference has been removed because it is automatically included in the
reference search.

The references are exported in the Taginfo output as well.

proxy/js/ui.js
@@ -101,6 +101,14 @@ const icons = {
   }
 }
 
+function naturalSort(a, b) {
+  return (a < b)
+    ? -1
+    : (a > b)
+      ? 1
+      : 0;
+}
+
 function facilitySearchUrl(type, term, language) {
   const url = new URL(`${location.origin}/api/facility`)
 
@@ -115,10 +123,6 @@ function facilitySearchUrl(type, term, language) {
 
     case 'ref':
       url.searchParams.set('ref', term)
-      break;
-
-    case 'uic_ref':
-      url.searchParams.set('uic_ref', term)
       break;
 
     case 'all':
@@ -139,6 +143,7 @@ function searchForFacilities(type, term, language) {
         ...item,
         label: [...new Set([item.localized_name, item.name])].join(' • '),
         icon: icons.railway[item.railway] ?? null,
+        references: item.references || {},
       })))
       .then(result => {
         showSearchResults(result)
@@ -161,6 +166,7 @@ function searchForMilestones(ref, position) {
         ...item,
         label: `Line ${item.line_ref} @ ${item.position}`,
         icon: icons.railway[item.railway] ?? null,
+        references: {},
       })))
       .then(result => {
         showSearchResults(result)
@@ -184,6 +190,7 @@ function showSearchResults(results) {
     ).toArray())
     : null;
 
+
   searchResults.innerHTML = results.length === 0
     ? `
       <div class="mb-1 d-flex align-items-center">
@@ -203,12 +210,22 @@ function showSearchResults(results) {
         </button>
       </div>
       <div class="list-group">
-        ${results.map(result =>
-      `<a class="list-group-item list-group-item-action" href="javascript:hideSearchResults(); map.easeTo({center: [${result.latitude}, ${result.longitude}], zoom: 15}); hideSearch()">
+        ${results.map(result => {
+          const catalog = (features ?? {}).station_references ?? {}
+          const sortKey = value => (catalog.features[value] ?? {}).index ?? Number.MAX_SAFE_INTEGER;
+          const references = Object.entries(result.references)
+            .toSorted(([keyA, _a], [keyB, _b]) => 
+              naturalSort(sortKey(keyA), sortKey(keyB)))
+            .map(([key, ref]) => 
+              `<span class="badge bg-secondary small">${(catalog.features[key] ?? {}).name ?? key}: ${ref}</span>`)
+            .join(' ')
+
+          return `<a class="list-group-item list-group-item-action" href="javascript:hideSearchResults(); map.easeTo({center: [${result.latitude}, ${result.longitude}], zoom: 15}); hideSearch()">
             ${result.icon ? `${result.icon}` : ''}
             ${result.label}
+            ${references}
           </a>`
-    ).join('')}
+        }).join('')}
       </div>
     `;
   searchResults.style.display = 'block';
@@ -2305,52 +2322,79 @@ function popupContent(feature) {
     })
   }
 
-  const formatPropertyValue = (value, format) =>
-    String(value)
-      .split('\u001e')
-      .map(stringValue => {
-        if (!format) {
-          return stringValue;
-        } else if (format.template) {
-          return format.template.replace('%s', () => stringValue).replace(/%(\.(\d+))?d/, (_1, _2, decimals) => Number(value).toFixed(Number(decimals)));
-        } else if (format.lookup) {
-          const lookupCatalog = features && features[format.lookup];
-          if (!lookupCatalog) {
-            console.warn('Lookup catalog', format.lookup, 'not found for feature', feature);
+  const formatPropertyValue = (value, format) => {
+    if (format && format.map) {
+      let sortKey = value => value;
+      if (format.map.key.format && format.map.key.format.lookup && features && features[format.map.key.format.lookup]) {
+        const catalog = features[format.map.key.format.lookup].features ?? {}
+        sortKey = value => (catalog[value] ?? {}).index ?? Number.MAX_SAFE_INTEGER;
+      }
+
+      return String(value)
+        .split('\u001d')
+        .map(item => item.split('\u001e'))
+        .toSorted(([keyA, _a], [keyB, _b]) =>
+          naturalSort(sortKey(keyA), sortKey(keyB)))
+        .map(([key, value]) =>
+          [formatPropertyValue(key, format.map.key.format), formatPropertyValue(value, format.map.value.format)])
+    } else {
+      return String(value)
+        .split('\u001e')
+        .map(stringValue => {
+          if (!format) {
             return stringValue;
-          } else {
-            const {catalogKey: lookUpCatalogKey, keyVariable: lookUpKeyVariable} = constructCatalogKey(value);
-            const lookedUpValue = lookupCatalog.features[lookUpCatalogKey];
-            if (!lookedUpValue) {
-              console.warn(`Lookup catalog ${format.lookup} did not contain key ${value} (catalog key ${lookUpCatalogKey}${lookUpKeyVariable ? ` with variable ${lookUpKeyVariable}`: ''}) for feature`, feature);
+          } else if (format.template) {
+            return format.template.replace('%s', () => stringValue).replace(/%(\.(\d+))?d/, (_1, _2, decimals) => Number(value).toFixed(Number(decimals)));
+          } else if (format.lookup) {
+            const lookupCatalog = features && features[format.lookup];
+            if (!lookupCatalog) {
+              console.warn('Lookup catalog', format.lookup, 'not found for feature', feature);
               return stringValue;
             } else {
-              return `${lookedUpValue.name}${lookUpKeyVariable ? ` (${lookUpKeyVariable})` : ''}${lookedUpValue.country ? ` ${getFlagEmoji(lookedUpValue.country)}` : ''}`;
+              const {catalogKey: lookUpCatalogKey, keyVariable: lookUpKeyVariable} = constructCatalogKey(value);
+              const lookedUpValue = lookupCatalog.features[lookUpCatalogKey];
+              if (!lookedUpValue) {
+                console.warn(`Lookup catalog ${format.lookup} did not contain key ${value} (catalog key ${lookUpCatalogKey}${lookUpKeyVariable ? ` with variable ${lookUpKeyVariable}`: ''}) for feature`, feature);
+                return stringValue;
+              } else {
+                return `${lookedUpValue.name}${lookUpKeyVariable ? ` (${lookUpKeyVariable})` : ''}${lookedUpValue.country ? ` ${getFlagEmoji(lookedUpValue.country)}` : ''}`;
+              }
             }
-          }
-        } else if (format.country_prefix) {
-          if (stringValue && stringValue.length >= 3 && stringValue[2] == ':') {
-            return stringValue.substr(3);
+          } else if (format.country_prefix) {
+            if (stringValue && stringValue.length >= 3 && stringValue[2] == ':') {
+              return stringValue.substr(3);
+            } else {
+              return stringValue;
+            }
           } else {
             return stringValue;
           }
-        } else {
-          return stringValue;
-        }
-      })
-      .join(', ');
+        })
+        .join(', ');
+    }
+  }
 
   const propertyValues = Object.entries(featureCatalog.properties || {})
     .filter(([property, {name, format, link}]) => (properties[property] !== undefined && properties[property] !== null && properties[property] !== '' && properties[property] !== false))
-    .map(([property, {name, format, link, paragraph, list, description}]) => ({
-      title: name,
-      value: properties[property],
-      body: properties[property] === true ? '' : formatPropertyValue(properties[property], format),
-      paragraph,
-      list,
-      link,
-      tooltip: description,
-    }));
+    .map(([property, {name, format, link, paragraph, list, description}]) => {
+      const value = properties[property] === true
+        ? ''
+        : formatPropertyValue(properties[property], format)
+
+      const body = Array.isArray(value)
+        ? value
+        : [[null, value]]
+
+      return {
+        title: name,
+        value: properties[property],
+        body,
+        paragraph,
+        list,
+        link,
+        tooltip: description,
+      };
+    })
 
   const osmFeatures = determineOsmFeatures(properties, featureContent);
 
@@ -2469,30 +2513,42 @@ function popupContent(feature) {
     propertyValues
       .filter(it => !it.paragraph && !it.list)
       .forEach(({title, body, value, link, tooltip}) => {
-        const popupValue = createDomElement('span', 'badge rounded-pill text-bg-light', popupValuesContainer);
+        const popupValue = createDomElement('span', 'badge fw-normal text-bg-light', popupValuesContainer);
         if (tooltip) {
           popupValue.title = tooltip;
           popupValue.style.cursor = 'help';
         }
 
         const popupValueTitle = createDomElement('span', 'fw-bold', popupValue);
-        popupValueTitle.innerText = title;
+        popupValueTitle.innerText = `${title}: `;
 
-        if (body) {
-          if (link) {
-            const popupValueBody = createDomElement('span', undefined, popupValue);
-            const popupValueColon = createDomElement('span', undefined, popupValueBody);
-            popupValueColon.innerText = ': ';
-            const popupValueLink = createDomElement('a', undefined, popupValueBody);
-            popupValueLink.href = link.replace('%s', () => encodeURIComponent(String(value)))
-            popupValueLink.target = '_blank'
-            const popupValueText = createDomElement('span', undefined, popupValueLink);
-            popupValueText.innerText = body;
-          } else {
-            const popupValueBody = createDomElement('span', undefined, popupValue);
-            popupValueBody.innerText = `: ${body}`;
+        let first = true
+        body.forEach(([key, value]) => {
+          if (value) {
+            if (first) {
+              first = false;
+            } else {
+              const popupValueKey = createDomElement('span', undefined, popupValue);
+              popupValueKey.innerText = ' • ';
+            }
+
+            if (key) {
+              const popupValueKey = createDomElement('span', 'fw-bold', popupValue);
+              popupValueKey.innerText = `${key} `;
+            }
+            if (link) {
+              const popupValueBody = createDomElement('span', undefined, popupValue);
+              const popupValueLink = createDomElement('a', undefined, popupValueBody);
+              popupValueLink.href = link.replace('%s', () => encodeURIComponent(String(value)))
+              popupValueLink.target = '_blank'
+              const popupValueText = createDomElement('span', undefined, popupValueLink);
+              popupValueText.innerText = value;
+            } else {
+              const popupValueBody = createDomElement('span', undefined, popupValue);
+              popupValueBody.innerText = value;
+            }
           }
-        }
+        })
       })
   }
 
@@ -2504,13 +2560,29 @@ function popupContent(feature) {
         const popupParagraph = createDomElement('p', undefined, popupValuesContainer);
 
         const popupValueTitle = createDomElement('span', 'fw-bold', popupParagraph);
-        popupValueTitle.innerText = title;
+        popupValueTitle.innerText = `${title}: `;
 
-        if (body) {
-          // Paragraph bodies do not support links
-          const popupValueBody = createDomElement('span', undefined, popupParagraph);
-          popupValueBody.innerText = `: ${body}`;
-        }
+        let first = true
+        body.forEach(([key, value]) => {
+          if (value) {
+            if (first) {
+              first = false;
+            } else {
+              const popupValueKey = createDomElement('span', undefined, popupParagraph);
+              popupValueKey.innerText = ' • ';
+            }
+
+            if (key) {
+              const popupValueKey = createDomElement('span', 'fw-bold', popupValue);
+              popupValueKey.innerText = `${key} `;
+            }
+            if (value) {
+              // Paragraph bodies do not support links
+              const popupValueBody = createDomElement('span', undefined, popupParagraph);
+              popupValueBody.innerText = value;
+            }
+          }
+        });
       })
   }
 

api/api.py
@@ -20,6 +20,9 @@ DEFAULT_HTTP_HEADERS = {
   'User-Agent': f'OpenRailwayMap API (https://openrailwaymap.app), httpx {httpx.__version__}, Python {sys.version}'
 }
 
+async def set_connection_codecs(conn):
+    await conn.set_builtin_type_codec('hstore', codec_name='pg_contrib.hstore')
+
 @contextlib.asynccontextmanager
 async def lifespan(app):
     async with asyncpg.create_pool(
@@ -29,6 +32,7 @@ async def lifespan(app):
             command_timeout=10,
             min_size=1,
             max_size=20,
+            init=set_connection_codecs,
     ) as pool:
         print('Connected to database')
         app.state.database = pool
@@ -76,12 +80,11 @@ async def facility(
         q: Annotated[str | None, Query()] = None,
         name: Annotated[str | None, Query()] = None,
         ref: Annotated[str | None, Query()] = None,
-        uic_ref: Annotated[str | None, Query()] = None,
         lang: Annotated[str | None, Query()] = None,
         limit: Annotated[int, Query(ge=MIN_LIMIT, le=MAX_LIMIT)] = DEFAULT_FACILITY_LIMIT,
 ):
     api = FacilityAPI(app.state.database)
-    return await api(q=q, name=name, ref=ref, uic_ref=uic_ref, limit=limit, language=lang)
+    return await api(q=q, name=name, ref=ref, limit=limit, language=lang)
 
 
 @app.get("/api/milestone")

api/openrailwaymap_api/facility_api.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_422_UNPROCESSABLE_ENTITY
 
-QUERY_PARAMETERS = ['q', 'name', 'ref', 'uic_ref']
+QUERY_PARAMETERS = ['q', 'name', 'ref']
 
 class FacilityAPI:
     def __init__(self, database):
@@ -21,9 +21,9 @@ class FacilityAPI:
 
         return results
 
-    async def __call__(self, *, q, name, ref, uic_ref, language, limit):
+    async def __call__(self, *, q, name, ref, language, limit):
         # Validate search arguments
-        search_args_count = sum(1 for search_arg in [q, name, ref, uic_ref] if search_arg)
+        search_args_count = sum(1 for search_arg in [q, name, ref] if search_arg)
 
         if search_args_count > 1:
             args = ', '.join(QUERY_PARAMETERS)
@@ -42,10 +42,8 @@ class FacilityAPI:
             return await self.search_by_name(name, language, limit)
         if ref:
             return await self.search_by_ref(ref, language, limit)
-        if uic_ref:
-            return await self.search_by_uic_ref(uic_ref, language, limit)
         if q:
-            return self.eliminate_duplicates((await self.search_by_name(q, language, limit)) + (await self.search_by_ref(q, language, limit)) + (await self.search_by_uic_ref(q, language, limit)), limit)
+            return self.eliminate_duplicates((await self.search_by_name(q, language, limit)) + (await self.search_by_ref(q, language, limit)), limit)
 
     def query_has_no_wildcards(self, q):
         if '%' in q or '_' in q:
@@ -72,9 +70,6 @@ class FacilityAPI:
 
     async def search_by_ref(self, ref, language, limit):
         return await self._search_by_ref("query_facilities_by_ref", ref, language, limit)
-
-    async def search_by_uic_ref(self, ref, language, limit):
-        return await self._search_by_ref("query_facilities_by_uic_ref", ref, language, limit)
 
     async def query_result(self, sql_query, parameters):
         async with self.database.acquire() as connection:

api/test/api.hurl
@@ -169,23 +169,36 @@ jsonpath "$[0].name" == "Berlin Ostkreuz (Ringbahn-F)"
 # Facility search for UIC reference
 GET {{base_url}}/facility
 [QueryStringParams]
-uic_ref: 8089028
+ref: 8089028
 HTTP 200
 [Asserts]
 jsonpath "$" count == 2
-jsonpath "$[0].name" == "Ostkreuz"
-jsonpath "$[1].name" == "Ostkreuz (Ringbahn)"
+jsonpath "$[0].name" == "Ostkreuz (Ringbahn)"
+jsonpath "$[1].name" == "Ostkreuz"
 
-# Facility search for name, reference or UIC reference
+# Facility search for BVG reference
+GET {{base_url}}/facility
+[QueryStringParams]
+ref: 309771
+HTTP 200
+[Asserts]
+jsonpath "$" count == 1
+jsonpath "$[0].name" == "Virchowstraße"
+jsonpath "$[0].references.BVG" == "309771"
+
+# Facility search for name or reference
 GET {{base_url}}/facility
 [QueryStringParams]
 q: BOK
 HTTP 200
 [Asserts]
-jsonpath "$" count == 1
+jsonpath "$" count == 4
 jsonpath "$[0].name" == "Ostkreuz (Ringbahn)"
+jsonpath "$[1].name" == "Rummelsburg"
+jsonpath "$[2].name" == "Frankfurter Allee"
+jsonpath "$[3].name" == "Treptower Park"
 
-# Facility search for name, reference or UIC reference
+# Facility search for name or reference
 GET {{base_url}}/facility
 [QueryStringParams]
 q: Berlin Ostkreuz
@@ -198,17 +211,17 @@ jsonpath "$[2].name" == "Berlin Ostkreuz (Ringbahn-F)"
 jsonpath "$[3].name" == "Berlin Ostkreuz (Ostbahn)"
 jsonpath "$[4].name" == "Ostkreuz (Ringbahn)"
 
-# Facility search for name, reference or UIC reference
+# Facility search for name or reference
 GET {{base_url}}/facility
 [QueryStringParams]
 q: 8089028
 HTTP 200
 [Asserts]
 jsonpath "$" count == 2
-jsonpath "$[0].name" == "Ostkreuz"
-jsonpath "$[1].name" == "Ostkreuz (Ringbahn)"
+jsonpath "$[0].name" == "Ostkreuz (Ringbahn)"
+jsonpath "$[1].name" == "Ostkreuz"
 
-# Facility search for name, reference or UIC reference for grouped station
+# Facility search for name or reference for grouped station
 GET {{base_url}}/facility
 [QueryStringParams]
 q: Landsberger Allee
@@ -238,7 +251,7 @@ GET {{base_url}}/facility
 HTTP 422
 [Asserts]
 jsonpath "$.detail.error" == "No argument with a search term provided."
-jsonpath "$.detail.detail" == "Provide one of the following query parameters: q, name, ref, uic_ref"
+jsonpath "$.detail.detail" == "Provide one of the following query parameters: q, name, ref"
 
 # Facility request duplicated search parameters (1)
 GET {{base_url}}/facility
@@ -248,39 +261,28 @@ ref: test
 HTTP 422
 [Asserts]
 jsonpath "$.detail.error" == "More than one argument with a search term provided."
-jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref, uic_ref"
+jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref"
 
 # Facility request duplicated search parameters (2)
 GET {{base_url}}/facility
 [QueryStringParams]
 q: 8089028
-uic_ref: test
+ref: test
 HTTP 422
 [Asserts]
 jsonpath "$.detail.error" == "More than one argument with a search term provided."
-jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref, uic_ref"
+jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref"
 
 # Facility request duplicated search parameters (3)
-GET {{base_url}}/facility
-[QueryStringParams]
-name: 8089028
-uic_ref: test
-HTTP 422
-[Asserts]
-jsonpath "$.detail.error" == "More than one argument with a search term provided."
-jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref, uic_ref"
-
-# Facility request duplicated search parameters (4)
 GET {{base_url}}/facility
 [QueryStringParams]
 q: test
 name: test
 ref: test
-uic_ref: test
 HTTP 422
 [Asserts]
 jsonpath "$.detail.error" == "More than one argument with a search term provided."
-jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref, uic_ref"
+jsonpath "$.detail.detail" == "Provide only one of the following query parameters: q, name, ref"
 
 # Facility request limit is not numeric
 GET {{base_url}}/facility

db/operators.sql
@@ -3,6 +3,23 @@ CREATE OR REPLACE FUNCTION commuted_regexp_match(text, text) RETURNS BOOL AS
   'SELECT $2 ~ $1;'
   LANGUAGE sql;
 
+-- Taken from https://stackoverflow.com/a/8142998/711129
+CREATE OR REPLACE FUNCTION unnest_nd_1d(a anyarray, OUT a_1d anyarray)
+  RETURNS SETOF anyarray
+  LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE STRICT AS
+$func$
+BEGIN
+  IF a = '{}' THEN
+    a_1d = '{}';
+    RETURN NEXT;
+  ELSE
+    FOREACH a_1d SLICE 1 IN ARRAY a LOOP
+      RETURN NEXT;
+    END LOOP;
+  END IF;
+END
+$func$;
+
 CREATE OPERATOR ~!@# (
   procedure = commuted_regexp_match(text, text),
   leftarg = text,

features/stations.yaml
@@ -116,3 +116,157 @@ features:
       station_size: small
       feature: crossover
       state: present
+
+references:
+  - id: railway-ref
+    description: Railway reference
+    map: true
+    tags: [ 'railway:ref' ]
+
+  - id: uic
+    description: UIC
+    tags: [ 'uic_ref' ]
+
+  - id: ibnr
+    description: IBNR
+    tags:
+      - 'ref:ibnr'
+      - 'ref:IBNR'
+
+  - id: ifopt
+    description: IFOPT
+    tags: [ 'ref:IFOPT' ]
+
+  - id: iata
+    description: IATA
+    tags: [ 'iata' ]
+
+  - id: eu-plc
+    description: PLC
+    country: EU
+    tags: [ 'ref:EU:PLC' ]
+
+  - id: fr-sncf-resarail
+    description: SNCF RESARAIL
+    country: FR
+    tags: [ 'ref:FR:sncf:resarail' ]
+
+  - id: fr-stif
+    description: STIF
+    country: FR
+    tags: [ 'ref:FR:STIF' ]
+
+  - id: fr-uic8
+    description: UIC8
+    country: FR
+    tags: [ 'ref:FR:uic8' ]
+
+  - id: fr-ratp
+    description: RATP
+    country: FR
+    tags: [ 'ref:FR:RATP' ]
+
+  - id: fr-fantoir
+    description: FANTOIR
+    country: FR
+    tags: [ 'ref:FR:FANTOIR' ]
+
+  - id: gb-crs
+    description: CRS
+    map: true
+    country: GB
+    tags: [ 'ref:crs' ]
+
+  - id: gb-tiploc
+    description: TIPLOC
+    country: GB
+    tags: [ 'ref:tiploc' ]
+
+  - id: hu-mav
+    description: MAV
+    country: HU
+    tags:
+      - 'ref:mav'
+      - 'ref:MAV'
+
+  - id: hu-bkk
+    description: BKK
+    country: HU
+    tags:
+      - 'ref:bkk'
+
+  - id: db-de
+    description: DB
+    country: DE
+    tags: [ 'railway:ref:DB' ]
+
+  - id: de-bvg
+    description: BVG
+    country: DE
+    tags: [ 'ref:BVG' ]
+
+  - id: de-vrs
+    description: VRS
+    country: DE
+    tags:
+      - 'ref:VRS'
+      - 'VRS:ref'
+
+  - id: de-mbv
+    description: MVB
+    country: DE
+    tags: [ 'ref:mvb' ]
+
+  - id: de-rnv-rbl
+    description: RNV RBL
+    country: DE
+    tags: [ 'ref:RNV:RBL' ]
+
+  - id: cz-pid
+    description: PID
+    country: CZ
+    tags: [ 'ref:PID' ]
+
+  - id: us-cif
+    description: CIF
+    country: US
+    tags: [ 'ref:US:CIF' ]
+
+  - id: us-meta
+    description: Meta
+    country: US
+    tags: [ 'ref:metra' ]
+
+  - id: nl-prorail
+    description: ProRail
+    country: NL
+    tags: [ 'ref:ProRailID' ]
+
+  - id: pl-ztm
+    description: ZTM
+    country: PL
+    tags: [ 'ref:ztm' ]
+
+  - id: ref
+    description: Reference
+    tags: [ 'ref' ]
+
+  - id: local
+    description: Local
+    tags: [ 'local_ref' ]
+
+  - id: old
+    description: Old
+    tags: [ 'old_ref' ]
+
+  - id: nat
+    description: National
+    tags: [ 'nat_ref' ]
+
+  - id: station
+    description: Station
+    tags: [ 'ref:station' ]
+
+  - id: parent
+    description: Parent
+    tags: [ 'railway:ref:parent' ]

import/Dockerfile
@@ -8,6 +8,7 @@ RUN --mount=type=bind,source=import/tags.lua.js,target=tags.lua.js \
   --mount=type=bind,source=features/train_protection.yaml,target=train_protection.yaml \
   --mount=type=bind,source=features/signals_railway_signals.yaml,target=signals_railway_signals.yaml \
   --mount=type=bind,source=features/poi.yaml,target=poi.yaml \
+  --mount=type=bind,source=features/stations.yaml,target=stations.yaml \
   node tags.lua.js \
     > /build/tags.lua
 

import/openrailwaymap.lua
@@ -253,11 +253,10 @@ local stations = osm2pgsql.define_table({
     { column = 'feature', type = 'text' },
     { column = 'state', type = 'text' },
     { column = 'name', type = 'text' },
-    { column = 'ref', type = 'text' },
     { column = 'station', type = 'text' },
-    { column = 'railway_ref', type = 'text' },
-    { column = 'uic_ref', type = 'text' },
     { column = 'name_tags', type = 'hstore' },
+    { column = 'map_reference', type = 'text' },
+    { column = 'references', type = 'hstore' },
     { column = 'operator', sql_type = 'text[]' },
     { column = 'network', sql_type = 'text[]' },
     { column = 'position', sql_type = 'text[]' },
@@ -276,8 +275,8 @@ local stations = osm2pgsql.define_table({
     -- For joining grouped_stations_with_importance with metadata from this table
     { column = 'id', method = 'btree', unique = true },
     { column = 'way', method = 'gist' },
-    { column = 'uic_ref', method = 'btree', where = 'uic_ref IS NOT NULL' },
-    { column = 'railway_ref', method = 'btree', where = 'railway_ref IS NOT NULL' },
+    -- Search by reference
+    { expression = 'avals("references")', method = 'gin', where = '"references" IS NOT NULL' },
   },
 })
 
@@ -816,6 +815,21 @@ function name_tags(tags)
   return found_name_tags
 end
 
+function station_references(tags)
+  local found_references = {}
+
+  for _, reference in ipairs(tag_functions.station_references) do
+    for _, tag in ipairs(reference.tags) do
+      if tags[tag] then
+        found_references[reference.id] = tags[tag]
+        break
+      end
+    end
+  end
+
+  return found_references
+end
+
 function position_is_zero(position)
   if position:find('^%-?%d+$') or position:find('^%-?%d*[,/.]0*$') then
     return true
@@ -1056,11 +1070,10 @@ function osm2pgsql.process_node(object)
         feature = station_feature,
         state = station_state,
         name = tags.name or tags.short_name,
-        ref = tags.ref,
         station = station,
-        railway_ref = tags['railway:ref'] or tags['ref:crs'],
-        uic_ref = tags['uic_ref'],
         name_tags = name_tags(tags),
+        map_reference = map_station_reference(tags),
+        references = station_references(tags),
         operator = split_semicolon_to_sql_array(tags.operator),
         network = split_semicolon_to_sql_array(tags.network),
         position = to_sql_array(map(parse_railway_positions(position, position_exact, line_positions), format_railway_position)),
@@ -1333,19 +1346,21 @@ function osm2pgsql.process_way(object)
 
   local station_feature, station_state = railway_feature_and_state(tags, railway_station_values)
   if station_feature then
+    local position, position_exact, line_positions = find_position_tags(tags)
+
     for station, _ in pairs(station_type(tags)) do
       stations:insert({
         way = object.is_closed and object:as_polygon() or object:as_linestring(),
         feature = station_feature,
         state = station_state,
         name = tags.name or tags.short_name,
-        ref = tags.ref,
         station = station,
-        railway_ref = tags['railway:ref'] or tags['ref:crs'],
-        uic_ref = tags['uic_ref'],
         name_tags = name_tags(tags),
+        map_reference = map_station_reference(tags),
+        references = station_references(tags),
         operator = split_semicolon_to_sql_array(tags.operator),
         network = split_semicolon_to_sql_array(tags.network),
+        position = to_sql_array(map(parse_railway_positions(position, position_exact, line_positions), format_railway_position)),
         yard_purpose = split_semicolon_to_sql_array(tags['railway:yard:purpose']),
         yard_hump = tags['railway:yard:hump'] == 'yes' or nil,
         wikidata = tags.wikidata,

commit d3c842bc072a6f059477ef0d59828be2159f557f

Fix Wikipedia links not working because of formatting (#852)
This is a regression from
https://github.com/hiddewie/OpenRailwayMap-vector/pull/832, noticed in
https://github.com/hiddewie/OpenRailwayMap-vector/pull/832#issuecomment-4041894532.

The variable `value` was overwritten by the formatted key/value pairs in
the body of the popup items.

After this pull request
<img width="642" height="382" alt="image"
src="https://github.com/user-attachments/assets/7446e820-2aef-42af-85b3-8e9b0b02a34a"
/>

proxy/js/ui.js
@@ -2523,8 +2523,8 @@ function popupContent(feature) {
         popupValueTitle.innerText = `${title}: `;
 
         let first = true
-        body.forEach(([key, value]) => {
-          if (value) {
+        body.forEach(([key, bodyValue]) => {
+          if (bodyValue) {
             if (first) {
               first = false;
             } else {
@@ -2542,10 +2542,10 @@ function popupContent(feature) {
               popupValueLink.href = link.replace('%s', () => encodeURIComponent(String(value)))
               popupValueLink.target = '_blank'
               const popupValueText = createDomElement('span', undefined, popupValueLink);
-              popupValueText.innerText = value;
+              popupValueText.innerText = bodyValue;
             } else {
               const popupValueBody = createDomElement('span', undefined, popupValue);
-              popupValueBody.innerText = value;
+              popupValueBody.innerText = bodyValue;
             }
           }
         })